1ae6fbceb3150b5a23938c535bf26f4af357099a
evilthingamajig <romir.chekuri@gmail.com>
2026-08-26 21:53:28 -0700

    perf(home): restore sharp stable assembly rendering

--- js/ff-assembly.js
+++ js/ff-assembly.js
@@ -12,7 +12,7 @@
     started = true;
     if (observer) observer.disconnect();
     root.dataset.assemblyLoading = '';
-    import('./home-candidate-assembly.js?v=candidate-28').catch(function (err) {
+    import('./home-candidate-assembly.js?v=candidate-29').catch(function (err) {
       delete root.dataset.assemblyLoading;
       console.warn('Homepage assembly:', err);
     });
